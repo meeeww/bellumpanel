1eb42992d18021691737e809df480d3e5e0d580e
Zas <thezasm03@gmail.com>
2024-02-12 20:01:09 +0100

    Fallback de permisos

--- src/utils/getPerms.js
+++ src/utils/getPerms.js
@@ -3,10 +3,12 @@ const getPerms = (rol) => {
     case 0:
       return "Usuario";
     case 1:
-      return "Jugador";
+      return "Privilegiado";
     case 2:
       return "Coach";
     case 3:
+      return "Admin";
+    default:
       return "Admin";
   }
 };
